02b566bc301d5b95469bfac63205e2a0f1ee09fe
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-03 15:14:13 +0100

    Aggiunta logica js che genera tramite funzione un <div> con le opportune classi css e ripete l'operazione tramite ciclo for inserendoli poi all'interno dell'elemento con id=gridContainer dell'html

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const startGameDom = document.querySelector('#startGame');
+const gridContainerDom = document.querySelector('#gridContainer');
+const boxNumber = 100;
+
+
+
+
+
+startGameDom.addEventListener('click', function(){
+    console.log(gridContainerDom.innerHTML);
+    gridContainerDom.innerHTML = '';
+    console.log(gridContainerDom.innerHTML);
+
+    for(i=1 ; i <= boxNumber ; i++){
+        const box = createNewBoxNumbered(i);
+        box.addEventListener('click', function(){
+            
+        });
+        gridContainerDom.append(box);
+    }
+});
+
+
+
+
+
+
+function createNewBoxNumbered(number){
+    const newBox = document.createElement('div');
+    newBox.classList.add('box');
+    newBox.innerHTML = number;
+    return newBox;
+}
